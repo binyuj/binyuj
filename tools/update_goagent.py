f37e444a136b84e6bf13961014d34ea5b12fbdae
binyu_J <yubinlove1991@gmail.com>
2013-07-08 18:41:31 +0800

    addMultithreading

--- tools/update_goagent.py
+++ tools/update_goagent.py
@@ -18,6 +18,27 @@ def main():
         print('Found appids:%s\n' % appids)
     except Exception:
         pass
+    '''
+    # 多线程处理
+    def updatefile(filename):
+        fileurl = 'https://github.com/goagent/goagent/raw/3.0/local/'+filename
+        print('Updating "%s" ... \n...from "%s"' % (filename, fileurl))
+        content = urllib.urlopen(fileurl).read()
+        target = open(filename, 'wb')
+        target.write(content)
+        target.close()
+        print('Successful updated "%s" !\n-----------------------------------' % filename)
+        if filename == 'goagent_gtk.py':
+            os.chmod(filename, 0755)
+        if filename == 'proxy.ini' and appids:
+            config.set('gae', 'appid', appids)
+            with open('proxy.ini', 'wb') as fp:
+                config.write(fp)
+            print('Successful wrote appids !')
+
+    for filename in filelist:
+        thread.start_new_thread(updatefile, (filename,))\
+    '''
 
     for filename in filelist:
         fileurl = 'https://github.com/goagent/goagent/raw/3.0/local/'+filename
