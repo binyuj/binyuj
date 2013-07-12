992f45f9d24332efee8db3e9dad620d1f4c50fc7
binyu_J <yubinlove1991@gmail.com>
2013-07-12 13:40:23 +0800

    support first connect

--- tools/auto_ssh.sh
+++ tools/auto_ssh.sh
@@ -2,8 +2,12 @@
 # ssh auto login script
 # need [expect] installed, sudo apt-get install expect
 
+set PASSWD yourpassword
 set timeout 20
 spawn ssh -N -D 7070 username@hostname.net
-expect "password:"
-send "yourpassword\r"
+expect {
+    "(yes/no)?" {send "yes\r"; exp_continue}
+    "password:" {send "$PASSWD\r"}
+}
+
 interact
