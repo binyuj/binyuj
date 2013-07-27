37dd122e3d173f36c316a8f7ca9ca8fcce5d3348
binyu_J <yubinlove1991@gmail.com>
2013-07-27 09:35:51 -0400

    fix thred error

--- tools/update_goagent_with_thread.py
+++ tools/update_goagent_with_thread.py
@@ -5,24 +5,20 @@
 
 import urllib
 import os
-import thread
+import threading
 
 filelist = ['proxy.ini', 'proxy.py', 'goagent-gtk.py', 'cacert.pem','addto-startup.py', 'proxy.pac']
 
-def main():
-    try:
-        import ConfigParser
-        import re
-        ConfigParser.RawConfigParser.OPTCRE = re.compile(r'(?P<option>[^=\s][^=]*)\s*(?P<vi>[=])\s*(?P<value>.*)$')
-        config = ConfigParser.ConfigParser()
-        config.read('proxy.ini')
-        appids = config.has_option('gae', 'appid') and config.get('gae', 'appid')
-        print('Found appids:%s\n' % appids)
-    except Exception:
-        pass
-    
-    # 多线程处理
-    def updatefile(filename):
+
+class Update(threading.Thread):
+    def __init__(self, filename):
+        threading.Thread.__init__(self)
+        self.filename = filename
+
+    def run(self):
+        self.updatefile(self.filename)
+
+    def updatefile(self, filename):
         fileurl = 'https://github.com/goagent/goagent/raw/3.0/local/'+filename
         print('Updating "%s" ... \n...from "%s"' % (filename, fileurl))
         content = urllib.urlopen(fileurl).read()
@@ -30,37 +26,33 @@ def main():
         target.write(content)
         target.close()
         print('Successful updated "%s" !' % filename)
-        if filename == 'goagent_gtk.py':
+
+        if filename == 'goagent-gtk.py':
             os.chmod(filename, 0755)
+            print('chmod '+filename)
         if filename == 'proxy.ini' and appids:
-            config.set('gae', 'appid', appids)
-            with open('proxy.ini', 'wb') as fp:
-                config.write(fp)
-            print('Successful wrote appids !')
+            try:
+                config.set('gae', 'appid', appids)
+                with open('proxy.ini', 'wb') as fp:
+                   config.write(fp)
+                print('Successful wrote appids !')
+            except Exception:
+                print('wrote appids failed (T＿T)')
+
+def main():
+    import ConfigParser
+    import re
+    global config
+    global appids
+    ConfigParser.RawConfigParser.OPTCRE = re.compile(r'(?P<option>[^=\s][^=]*)\s*(?P<vi>[=])\s*(?P<value>.*)$')
+    config = ConfigParser.ConfigParser()
+    config.read('proxy.ini')
+    appids = config.has_option('gae', 'appid') and config.get('gae', 'appid')
+    print('Found appids:%s\n' % appids)
 
     for filename in filelist:
-        thread.start_new_thread(updatefile, (filename,))
-
-    '''
-    
-    for filename in filelist:
-        fileurl = 'https://github.com/goagent/goagent/raw/3.0/local/'+filename
-        print('Updating "%s" ... \n...from "%s"' % (filename, fileurl))
-        content = urllib.urlopen(fileurl).read()
-        target = open(filename, 'wb')
-        target.write(content)
-        target.close()
-        print('Successful updated "%s" !\n-----------------------------------' % filename)
-        if filename == 'goagent_gtk.py':
-            os.chmod(filename, 0755)
-    print('All file up to date. Done!')
-
-    if appids:
-        config.set('gae', 'appid', appids)
-        with open('proxy.ini', 'wb') as fp:
-            config.write(fp)
-        print('Successful wrote appids !')
-    '''
+        thread = Update(filename)
+        thread.start()
 
 if __name__ == '__main__':
     try:
