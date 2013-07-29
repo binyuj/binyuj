232cba29b924ac122b89d6bae27eeb4d6bc32140
binyu_J <yubinlove1991@gmail.com>
2013-07-29 11:50:14 -0400

    cowork with py3

--- tools/update_goagent_with_thread.py
+++ tools/update_goagent_with_thread.py
@@ -5,7 +5,17 @@
 
 import urllib
 import os
+import re
 import threading
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+try:
+    import urllib.request
+except ImportError:
+    import urllib
+    urllib.request = __import__('urllib2')
 
 filelist = ['proxy.ini', 'proxy.py', 'goagent-gtk.py', 'cacert.pem','addto-startup.py', 'proxy.pac']
 
@@ -21,31 +31,29 @@ class Update(threading.Thread):
     def updatefile(self, filename):
         fileurl = 'https://github.com/goagent/goagent/raw/3.0/local/'+filename
         print('Updating "%s" ... \n...from "%s"' % (filename, fileurl))
-        content = urllib.urlopen(fileurl).read()
+        content = urllib.request.urlopen(fileurl).read()
         target = open(filename, 'wb')
         target.write(content)
         target.close()
         print('Successful updated "%s" !' % filename)
 
         if filename == 'goagent-gtk.py':
-            os.chmod(filename, 0755)
+            os.chmod(filename, 0o775)
             print('chmod '+filename)
         if filename == 'proxy.ini' and appids:
             try:
                 config.set('gae', 'appid', appids)
-                with open('proxy.ini', 'wb') as fp:
+                with open('proxy.ini', 'w') as fp:
                    config.write(fp)
                 print('Successful wrote appids !')
             except Exception:
                 print('wrote appids failed (T＿T)')
 
 def main():
-    import ConfigParser
-    import re
     global config
     global appids
-    ConfigParser.RawConfigParser.OPTCRE = re.compile(r'(?P<option>[^=\s][^=]*)\s*(?P<vi>[=])\s*(?P<value>.*)$')
-    config = ConfigParser.ConfigParser()
+    configparser.RawConfigParser.OPTCRE = re.compile(r'(?P<option>[^=\s][^=]*)\s*(?P<vi>[=])\s*(?P<value>.*)$')
+    config = configparser.ConfigParser()
     config.read('proxy.ini')
     appids = config.has_option('gae', 'appid') and config.get('gae', 'appid')
     print('Found appids:%s\n' % appids)
